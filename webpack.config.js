d38064e8947636956259e03cccd3020cc302e052
Luke Timothy <luke.timothy@gmail.com>
2019-08-06 10:23:50 +0100

    fix build

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+        })
+    ]
+}
